b75495733a9d54e8e99d9c9171e0139a850c81c9
misterelgo <misterelgo@gmail.com>
2020-03-26 15:12:37 +0000

    adding spotBugs

--- src/main/java/com/elgo/App.java
+++ src/main/java/com/elgo/App.java
@@ -11,6 +11,8 @@ public class App
         System.out.println( "Hello World!" );
         Employe employe1 = new Employe(2018);
         Vendeur vendeur1 = new Vendeur(200);
+        Employe employe2 = new Employe(2019);
+        Vendeur vendeur2 = new Vendeur(600);
         double salaireTotal = employe1.salaire() + vendeur1.salaire();
         System.out.println("Le salaire total est : "+ salaireTotal);
     }
